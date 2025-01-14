04ffc8fe4598243d60f91eec7af4c1306e14f768
ChaimaGhanney <chaimaghanney05@gmail.com>
2025-01-14 22:58:39 +0100

    db conexion

--- db/cnx.js
+++ db/cnx.js
@@ -4,10 +4,12 @@ import mongoose from 'mongoose'
 
 const connectDB = () => {
   //mongoose.connect('mongodb://127.0.0.1:27017/stock?retryWrites=true&w=majority&appName=stock')
-  mongoose.connect('mongodb+srv://elyes44:iXuBsQbuhLYjVAlF@stock.6bhh36q.mongodb.net/?retryWrites=true&w=majority&appName=stock')
+  mongoose.connect('mongodb+srv://chaimaghanney05:uwLReZAMlzKDsNgF@cluster0.tmx1k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
   .then(() => {
     console.log(`Db connected : ${process.env.MONGO_URI} `);
   })
   .catch((err) => { console.log(err) });
 }
   export default  connectDB; 
+
+
