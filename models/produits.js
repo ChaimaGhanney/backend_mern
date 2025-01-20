4b69c92948a70993d53b5a949737e598b8517639
ChaimaGhanney <chaimaghanney05@gmail.com>
2025-01-20 23:04:34 +0100

    test-final2

--- models/produits.js
+++ models/produits.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const produit = new mongoose.Schema({
     nom_produit: String,
     categorie_produit: { type: String, enum:["CDD", "CDI", "CIVP", "CSC"]},
-    etat: {type: String, enum:["Present", "Absent"]},
+    etat: {type: String, enum:["present", "absent"]},
     Marque: String,
     Prix: Number,
     Description: String,
