0ac6881962ebb7ada0494a44cf56d5fffafc614e
ChaimaGhanney <chaimaghanney05@gmail.com>
2025-01-20 20:19:19 +0100

    demo

--- models/produits.js
+++ models/produits.js
@@ -3,8 +3,8 @@ import Users from './users.js';
 const Schema = mongoose.Schema;
 const produit = new mongoose.Schema({
     nom_produit: String,
-    categorie_produit: { type: String, enum:["informatique", "bureatique", "logistique", "personnelle"]},
-    etat: {type: String, enum:["en_stock", "hors_stock"]},
+    categorie_produit: { type: String, enum:["CDD", "CDI", "CIVP", "CSC"]},
+    etat: {type: String, enum:["Present", "Absent"]},
     Marque: String,
     Prix: Number,
     Description: String,
